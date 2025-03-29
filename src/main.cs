3884b18d3440c50f47bf09ffe0756c0fb7092905
Kishan Vaghamashi <82988624+vaghamashikishan@users.noreply.github.com>
2025-03-29 09:55:01 +0530

    Addition: Add loop to continuously accept user commands

--- src/main.cs
+++ src/main.cs
@@ -1,3 +1,7 @@
-Console.Write("$ ");
-var command = Console.ReadLine();
-Console.WriteLine($"{command}: command not found");
+bool run = true;
+while (run)
+{
+    Console.Write("$ ");
+    var command = Console.ReadLine();
+    Console.WriteLine($"{command}: command not found");
+}
